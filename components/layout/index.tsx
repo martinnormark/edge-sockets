24c41f0de4c9fd53a8030f72d764f998d9812e11
Martin H. Normark <m@martinnormark.com>
2023-02-25 23:14:34 +0100

    Remove login

--- components/layout/index.tsx
+++ components/layout/index.tsx
@@ -6,7 +6,6 @@ import Link from "next/link";
 import { ReactNode } from "react";
 import useScroll from "@/lib/hooks/use-scroll";
 import Meta from "./meta";
-import { useSignInModal } from "./sign-in-modal";
 import UserDropdown from "./user-dropdown";
 
 export default function Layout({
@@ -21,13 +20,11 @@ export default function Layout({
   children: ReactNode;
 }) {
   const { data: session, status } = useSession();
-  const { SignInModal, setShowSignInModal } = useSignInModal();
   const scrolled = useScroll(50);
 
   return (
     <>
       <Meta {...meta} />
-      <SignInModal />
       <div className="fixed h-screen w-full bg-gradient-to-br from-indigo-50 via-white to-cyan-100" />
       <div
         className={`fixed top-0 w-full ${
@@ -47,21 +44,6 @@ export default function Layout({
             ></Image>
             <p>Precedent</p>
           </Link>
-          <div>
-            <AnimatePresence>
-              {!session && status !== "loading" ? (
-                <motion.button
-                  className="rounded-full border border-black bg-black p-1.5 px-4 text-sm text-white transition-all hover:bg-white hover:text-black"
-                  onClick={() => setShowSignInModal(true)}
-                  {...FADE_IN_ANIMATION_SETTINGS}
-                >
-                  Sign In
-                </motion.button>
-              ) : (
-                <UserDropdown />
-              )}
-            </AnimatePresence>
-          </div>
         </div>
       </div>
       <main className="flex w-full flex-col items-center justify-center py-32">
